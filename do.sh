e3a70e776773285c9aa4a1c8a82bd9cee41f1740
Olivier Vignaud <ovi@ubiqube.com>
2023-10-11 14:22:50 +0200

    Do not apply command on .* folders.

--- do.sh
+++ do.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-for d in $(find . -maxdepth 1 -type d)
+for d in $(find . -maxdepth 1 -type d -name 'etsi*')
 do
 	echo -e "======================================== \033[32;1;1m$d\033[0m ========================================"
 	pushd $d
